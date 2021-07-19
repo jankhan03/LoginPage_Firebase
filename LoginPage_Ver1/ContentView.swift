15bdb5bf9cd4b2ac238c9f3ddef8bbcd2860d147
Yan <jankhan03@mail.ru>
2021-07-19 13:20:21 +0300

    Initial Commit

---
+++ LoginPage_Ver1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginPage_Ver1
+//
+//  Created by Yan Khanetski on 19.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginPage_Ver1/LoginPage_Ver1App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPage_Ver1App.swift
+//  LoginPage_Ver1
+//
+//  Created by Yan Khanetski on 19.07.21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPage_Ver1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
